07fd1b690a7d67d24d4ddf4ca5395caa8c481a1b
anchen li <anchen.li@xero.com>
2017-08-22 15:14:10 +1000

    fix setState during rendering

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import glamorous from 'glamorous';
 const checkWindowPositionForContainer = ({ containerPosition, containerSize, positionMargin, position }) => {
   if (position === 'top' || position === 'bottom') {
     if (containerPosition.left < 0) {
-      return { ...containerPosition, left: positionMargin }
-    } else {
-      const rightOffset = containerPosition.left + containerSize.width - window.innerWidth;
-      if (rightOffset > 0) {
-        return { ...containerPosition, left: (window.innerWidth - containerSize.width - positionMargin) }
-      }
+      return { ...containerPosition, left: positionMargin };
+    }
+
+    const rightOffset = containerPosition.left + containerSize.width - window.innerWidth;
+    if (rightOffset > 0) {
+      return { ...containerPosition, left: window.innerWidth - containerSize.width - positionMargin };
     }
   }
   return containerPosition;
@@ -18,21 +18,22 @@ const checkWindowPositionForContainer = ({ containerPosition, containerSize, pos
 
 const getContainerPosition = ({ position, arrow, targetElm, containerSize, positionMargin }) => {
   const targetElmPosition = targetElm.getBoundingClientRect();
-  const scrollY = (window.scrollY !== undefined) ? window.scrollY : window.pageYOffset;
-  const scrollX = (window.scrollX !== undefined) ? window.scrollX : window.pageXOffset;
+  const scrollY = window.scrollY !== undefined ? window.scrollY : window.pageYOffset;
+  const scrollX = window.scrollX !== undefined ? window.scrollX : window.pageXOffset;
   const top = scrollY + targetElmPosition.top;
   const left = scrollX + targetElmPosition.left;
   const leftWhenPositionLeft = left - containerSize.width - positionMargin;
   const leftWhenPositionRight = left + targetElm.offsetWidth + positionMargin;
-  const topWhenPositionLeftOrRightAndArrowTop = (top + (targetElm.offsetHeight / 2)) - positionMargin;
-  const topWhenPositionLeftOrRightAndArrowBottom = (top + targetElm.offsetHeight / 2) - containerSize.height + positionMargin;
-  const topWhenPositionLeftOrRightAndArrowCenter = (top + targetElm.offsetHeight / 2) - ((containerSize.height) / 2);
-  const leftWhenPositionTopAndArrowCenter = left - (containerSize.width / 2) + targetElm.offsetWidth / 2;
+  const topWhenPositionLeftOrRightAndArrowTop = top + targetElm.offsetHeight / 2 - positionMargin;
+  const topWhenPositionLeftOrRightAndArrowBottom =
+    top + targetElm.offsetHeight / 2 - containerSize.height + positionMargin;
+  const topWhenPositionLeftOrRightAndArrowCenter = top + targetElm.offsetHeight / 2 - containerSize.height / 2;
+  const leftWhenPositionTopAndArrowCenter = left - containerSize.width / 2 + targetElm.offsetWidth / 2;
   const leftWhenPositionTopAndArrowLeft = left + targetElm.offsetWidth / 2 - positionMargin;
   const leftWhenPositionTopAndArrowRight = left - containerSize.width + targetElm.offsetWidth / 2 + positionMargin;
   const topWhenPositionTopAndArrowCenterOrRightOrLeft = top - containerSize.height - positionMargin;
   const topWhenPositionBottomAndArrowCenterOrRightOrLeft = top + targetElm.offsetHeight + positionMargin;
-  const leftWhenPositionBottomAndArrowCenter = left - (containerSize.width / 2) + targetElm.offsetWidth / 2;
+  const leftWhenPositionBottomAndArrowCenter = left - containerSize.width / 2 + targetElm.offsetWidth / 2;
   const leftWhenPositionBottomAndArrowLeft = left + targetElm.offsetWidth / 2 - positionMargin;
   const leftWhenPositionBottomAndArrowRight = left - containerSize.width + targetElm.offsetWidth / 2 + positionMargin;
 
@@ -116,166 +117,156 @@ const getContainerPosition = ({ position, arrow, targetElm, containerSize, posit
 };
 const isOffScreen = (position, containerPosition, containerSize) => {
   let offScreen = false;
-  if (position === "left" && containerPosition.left < 0) {
+  if (position === 'left' && containerPosition.left < 0) {
     offScreen = true;
   }
-  if (
-    position === "right" &&
-    window.innerWidth < containerPosition.left + containerSize.width
-  ) {
+  if (position === 'right' && window.innerWidth < containerPosition.left + containerSize.width) {
     offScreen = true;
   }
   return offScreen;
 };
-const ContainerDiv = glamorous.div((props) => {
-    const { transition, visible, reposition, style, ...restProps } = props;
-    const { targetElm, position, containerSize } = restProps;
-    if (!targetElm) return { display: 'none' };
-    const baseStyle = {
-      position: 'absolute',
-      padding: '5px',
-      background: '#fff',
-      boxShadow: '0 0 8px rgba(0,0,0,.3)',
-      borderRadius: '3px',
-      transition: `${transition} .3s ease-in-out, visibility .3s ease-in-out`,
-      opacity: visible ? 1 : 0,
-      visibility: visible ? 'visible' : 'hidden',
-      zIndex: 50,
-      ...style,
+const ContainerDiv = glamorous.div(props => {
+  const { transition, visible, setIsOffScreen, style, ...restProps } = props;
+  const { targetElm, position, containerSize } = restProps;
+  if (!targetElm) return { display: 'none' };
+  const baseStyle = {
+    position: 'absolute',
+    padding: '5px',
+    background: '#fff',
+    boxShadow: '0 0 8px rgba(0,0,0,.3)',
+    borderRadius: '3px',
+    transition: `${transition} .3s ease-in-out, visibility .3s ease-in-out`,
+    opacity: visible ? 1 : 0,
+    visibility: visible ? 'visible' : 'hidden',
+    zIndex: 50,
+    ...style,
+  };
+  const containerPosition = getContainerPosition(restProps);
+  const checkedContainerPosition = checkWindowPositionForContainer({ ...restProps, containerPosition });
+  setIsOffScreen(isOffScreen(position, checkedContainerPosition, containerSize));
+  return { ...baseStyle, ...checkedContainerPosition };
+});
+
+const Arrow = glamorous.span(props => {
+  const { fg, bg, position, arrow, arrowStyle } = props;
+  const baseStyle = {
+    position: 'absolute',
+    content: '""',
+    transition: 'all .3s ease-in-out',
+  };
+  let fgStyle = {};
+  let bgStyle = {};
+  const newArrowStyle = { color: '#fff', borderColor: 'rgba(0,0,0,.4)', ...arrowStyle };
+  if (fg) {
+    fgStyle.zIndex = 60;
+    const fgColorBorder = `10px solid ${newArrowStyle.color}`;
+    const fgTransBorder = '8px solid transparent';
+    const fgStyleLeftOrRightBase = {
+      borderTop: fgTransBorder,
+      borderBottom: fgTransBorder,
+      top: 'calc(50% - 8px)',
     };
-    const containerPosition = getContainerPosition(restProps);
-    const checkedContainerPosition = checkWindowPositionForContainer({ ...restProps, containerPosition });
-    if (isOffScreen(position, checkedContainerPosition, containerSize)) reposition();
+    if (position === 'left') {
+      fgStyle = { ...fgStyle, ...fgStyleLeftOrRightBase, right: -10, borderLeft: fgColorBorder };
+    }
+    if (position === 'right') {
+      fgStyle = { ...fgStyle, ...fgStyleLeftOrRightBase, left: -10, borderRight: fgColorBorder };
+    }
+    if (position === 'left' || position === 'right') {
+      if (arrow === 'top') {
+        fgStyle.top = 2;
+      }
+      if (arrow === 'bottom') {
+        fgStyle.top = null;
+        fgStyle.bottom = 1;
+      }
+    }
 
-    return { ...baseStyle, ...checkedContainerPosition };
-  },
-);
-
-const Arrow = glamorous.span((props) => {
-    const { fg, bg, position, arrow, arrowStyle } = props;
-    const baseStyle = {
-      position: 'absolute',
-      content: '""',
-      transition: 'all .3s ease-in-out',
+    const fgStyleTopOrBottomBase = {
+      left: '50%',
+      marginLeft: -8,
+      borderLeft: fgTransBorder,
+      borderRight: fgTransBorder,
     };
-    let fgStyle = {};
-    let bgStyle = {};
-    const newArrowStyle = { color: '#fff', borderColor: 'rgba(0,0,0,.4)', ...arrowStyle };
-    if (fg) {
-      fgStyle.zIndex = 60;
-      const fgColorBorder = `10px solid ${newArrowStyle.color}`;
-      const fgTransBorder = '8px solid transparent';
-      const fgStyleLeftOrRightBase = {
-        borderTop: fgTransBorder,
-        borderBottom: fgTransBorder,
-        marginTop: '50%',
-      };
-      if (position === 'left') {
-        fgStyle = { ...fgStyle, ...fgStyleLeftOrRightBase, right: -10, top: 5, borderLeft: fgColorBorder };
-      }
-      if (position === 'right') {
-        fgStyle = { ...fgStyle, ...fgStyleLeftOrRightBase, left: -10, top: 5, borderRight: fgColorBorder };
-      }
-      if (position === 'left' || position === 'right') {
-        if (arrow === 'top') {
-          fgStyle.marginTop = null;
-          fgStyle.top = null;
-        }
-        if (arrow === 'bottom') {
-          fgStyle.top = null;
-          fgStyle.marginTop = null;
-          fgStyle.bottom = 2;
-        }
-      }
-
-      const fgStyleTopOrBottomBase = {
-        left: '50%',
-        marginLeft: -8,
-        borderLeft: fgTransBorder,
-        borderRight: fgTransBorder,
-      };
-      if (position === 'top') {
-        fgStyle = { ...fgStyle, ...fgStyleTopOrBottomBase, bottom: -10, borderTop: fgColorBorder };
-      }
-      if (position === 'bottom') {
-        fgStyle = { ...fgStyle, ...fgStyleTopOrBottomBase, top: -10, borderBottom: fgColorBorder };
-      }
-      if (position === 'top' || position === 'bottom') {
-        if (arrow === 'right') {
-          fgStyle.left = null;
-          fgStyle.right = 2;
-          fgStyle.marginLeft = null;
-        }
-        if (arrow === 'left') {
-          fgStyle.left = null;
-          fgStyle.marginLeft =null;
-        }
-      }
-      return { ...baseStyle, ...fgStyle };
+    if (position === 'top') {
+      fgStyle = { ...fgStyle, ...fgStyleTopOrBottomBase, bottom: -10, borderTop: fgColorBorder };
     }
-    if (bg) {
-      bgStyle.zIndex = 55;
-      const bgBorderColor = newArrowStyle.borderColor ? newArrowStyle.borderColor : 'transparent';
-      const bgColorBorder = `11px solid ${bgBorderColor}`;
-      const bgTransBorder = '9px solid transparent';
-      const bgStyleLeftOrRightBase = {
-        borderTop: bgTransBorder,
-        borderBottom: bgTransBorder,
-        marginTop: '50%',
-      };
-      if (position === 'left') {
-        bgStyle = { ...bgStyle, ...bgStyleLeftOrRightBase, right: -11, top: 5, borderLeft: bgColorBorder };
-      }
-      if (position === 'right') {
-        bgStyle = { ...bgStyle, ...bgStyleLeftOrRightBase, left: -11, top: 5, borderRight: bgColorBorder };
-      }
-      if (position === 'left' || position === 'right') {
-        if (arrow === 'top') {
-          bgStyle.marginTop = null;
-          bgStyle.top = null;
-        }
-        if (arrow === 'bottom') {
-          bgStyle.top = null;
-          bgStyle.marginTop = null;
-          bgStyle.bottom = 0;
-        }
-      }
-      const bgStyleTopOrBottomBase = {
-        left: '50%',
-        marginLeft: -8,
-        borderLeft: bgTransBorder,
-        borderRight: bgTransBorder,
-      };
-      if (position === 'top') {
-        bgStyle = { ...bgStyle, ...bgStyleTopOrBottomBase, bottom: -11, borderTop: bgColorBorder };
-      }
-      if (position === 'bottom') {
-        bgStyle = { ...bgStyle, ...bgStyleTopOrBottomBase, top: -11, borderBottom: bgColorBorder };
-      }
-      if (position === 'top' || position === 'bottom') {
-        if (arrow === 'right') {
-          bgStyle.left = null;
-          bgStyle.right = 0;
-          bgStyle.marginLeft = null;
-        }
-        if (arrow === 'left') {
-          bgStyle.left = null;
-          bgStyle.marginLeft = null;
-        }
-      }
-      return { ...baseStyle, ...bgStyle };
+    if (position === 'bottom') {
+      fgStyle = { ...fgStyle, ...fgStyleTopOrBottomBase, top: -10, borderBottom: fgColorBorder };
     }
-    return null;
-  },
-);
+    if (position === 'top' || position === 'bottom') {
+      if (arrow === 'right') {
+        fgStyle.left = null;
+        fgStyle.right = '0px';
+        fgStyle.marginLeft = null;
+      }
+      if (arrow === 'left') {
+        fgStyle.left = null;
+        fgStyle.marginLeft = null;
+      }
+    }
+    return { ...baseStyle, ...fgStyle };
+  }
+  if (bg) {
+    bgStyle.zIndex = 55;
+    const bgBorderColor = newArrowStyle.borderColor ? newArrowStyle.borderColor : 'transparent';
+    const bgColorBorder = `11px solid ${bgBorderColor}`;
+    const bgTransBorder = '9px solid transparent';
+    const bgStyleLeftOrRightBase = {
+      borderTop: bgTransBorder,
+      borderBottom: bgTransBorder,
+      top: 'calc(50% - 9px)',
+    };
+    if (position === 'left') {
+      bgStyle = { ...bgStyle, ...bgStyleLeftOrRightBase, right: -11, borderLeft: bgColorBorder };
+    }
+    if (position === 'right') {
+      bgStyle = { ...bgStyle, ...bgStyleLeftOrRightBase, left: -11, borderRight: bgColorBorder };
+    }
+    if (position === 'left' || position === 'right') {
+      if (arrow === 'top') {
+        bgStyle.top = 1;
+      }
+      if (arrow === 'bottom') {
+        bgStyle.top = null;
+        bgStyle.bottom = 0;
+      }
+    }
+    const bgStyleTopOrBottomBase = {
+      left: '50%',
+      marginLeft: -9,
+      borderLeft: bgTransBorder,
+      borderRight: bgTransBorder,
+    };
+    if (position === 'top') {
+      bgStyle = { ...bgStyle, ...bgStyleTopOrBottomBase, bottom: -11, borderTop: bgColorBorder };
+    }
+    if (position === 'bottom') {
+      bgStyle = { ...bgStyle, ...bgStyleTopOrBottomBase, top: -11, borderBottom: bgColorBorder };
+    }
+    if (position === 'top' || position === 'bottom') {
+      if (arrow === 'right') {
+        bgStyle.left = null;
+        bgStyle.right = -1;
+        bgStyle.marginLeft = null;
+      }
+      if (arrow === 'left') {
+        bgStyle.left = -1;
+        bgStyle.marginLeft = null;
+      }
+    }
+    return { ...baseStyle, ...bgStyle };
+  }
+  return null;
+});
 const ArrowContainer = glamorous.div({
   position: 'absolute',
   top: 0,
   left: 0,
   right: 0,
-  bottom: 0
-
+  bottom: 0,
 });
+
 class ToolTip extends Component {
   constructor(props) {
     super(props);
@@ -283,7 +274,8 @@ class ToolTip extends Component {
       position: props.position,
       arrow: props.arrow,
     };
-    this.reposition = this.reposition.bind(this);
+    this.setIsOffScreen = this.setIsOffScreen.bind(this);
+    this.isOffScreen = false;
   }
 
   componentDidMount() {
@@ -297,12 +289,19 @@ class ToolTip extends Component {
     });
   }
 
-  reposition() {
-    const { fallbackPosition, fallbackArrow } = this.props;
-    this.setState({
-      position: fallbackPosition,
-      arrow: fallbackArrow
-    });
+  componentDidUpdate() {
+    if (this.isOffScreen) {
+      const { fallbackPosition, fallbackArrow } = this.props;
+      // prettier-ignore
+      this.setState({ // eslint-disable-line react/no-did-update-set-state
+        position: fallbackPosition,
+        arrow: fallbackArrow,
+      });
+    }
+  }
+
+  setIsOffScreen(offScreen) {
+    this.isOffScreen = offScreen;
   }
 
   getContainerSize() {
@@ -327,7 +326,7 @@ class ToolTip extends Component {
       <ContainerDiv
         style={containerStyle}
         visible={visible}
-        innerRef={(ref) => {
+        innerRef={ref => {
           this.ref = ref;
         }}
         transition={transition}
@@ -337,26 +336,14 @@ class ToolTip extends Component {
         containerSize={this.getContainerSize()}
         positionMargin={positionMargin}
         reposition={this.reposition}
+        setIsOffScreen={this.setIsOffScreen}
       >
-        {this.props.arrow ? (
-          <ArrowContainer>
-            <Arrow
-              fg
-              arrowStyle={arrowStyle}
-              position={position}
-              arrow={arrow}
-              positionMargin={positionMargin}
-            />
-            <Arrow
-              bg
-              arrowStyle={arrowStyle}
-              position={position}
-              arrow={arrow}
-              positionMargin={positionMargin}
-            />
-          </ArrowContainer>)
-          : null
-        }
+        {this.props.arrow
+          ? <ArrowContainer>
+            <Arrow fg arrowStyle={arrowStyle} position={position} arrow={arrow} positionMargin={positionMargin} />
+            <Arrow bg arrowStyle={arrowStyle} position={position} arrow={arrow} positionMargin={positionMargin} />
+          </ArrowContainer>
+          : null}
         {this.props.children}
       </ContainerDiv>
     );
@@ -365,12 +352,12 @@ class ToolTip extends Component {
 
 ToolTip.propTypes = {
   visible: PropTypes.bool,
-  targetElm: PropTypes.object,
+  targetElm: PropTypes.object, // eslint-disable-line
   position: PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
   fallbackPosition: PropTypes.oneOf(['top', 'right', 'bottom', 'left']),
   arrow: PropTypes.oneOf([null, 'center', 'top', 'right', 'bottom', 'left']),
   fallbackArrow: PropTypes.oneOf([null, 'center', 'top', 'right', 'bottom', 'left']),
-  style: PropTypes.object,
+  style: PropTypes.object, // eslint-disable-line
   positionMargin: PropTypes.number,
   transition: PropTypes.string,
   children: PropTypes.element.isRequired,
